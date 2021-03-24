360f153a2c2fc65a8c63fbf194ef15252a75d2ac
Seoyeon622 <79996353+Seoyeon622@users.noreply.github.com>
2021-03-25 00:23:55 +0900

    2021.03.25

--- 9498.java
+++ 9498.java
@@ -4,7 +4,6 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int score = scan.nextInt();
-		//int b = scan.nextInt();
 		
 		if(score>=90)
 		{
